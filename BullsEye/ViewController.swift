2337edfc6846d430b4123b14137dc8c3f61880f8
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-10-15 23:59:14 +0300

    add slider functional

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -1,27 +1,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue: Int = 0
+    @IBOutlet var slider: UISlider!
+    var targetValue = 0
+    @IBOutlet var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        startNewRound()
     }
 
     @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)" +
+        "\nThe target value is: \(targetValue)"
+
         let alert = UIAlertController(
             title: "Hello, World",
-            message: "This is my first APP",
+            message: message,
             preferredStyle: .alert)
 
         let action = UIAlertAction(
-            title: "Awsome",
+            title: "OK",
             style: .default,
             handler: nil)
 
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
 
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+    }
 
 }
 
